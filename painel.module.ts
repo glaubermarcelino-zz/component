e552ca37403eccdb6fe7652eebadc1746bfca9f0
Glauber M. da Silva <gmsoft.informatica@gmail.com>
2018-02-15 19:20:23 -0300

    Adicionado a dependencia @angular/core
    Adicionado a dependência @angular/core para o caso de inclusão de module

---
+++ painel.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from './../gts/node_modules/@angular/core';
+import { PainelComponent } from './painel/painel.component';
+
+
+@NgModule({
+  declarations: [
+    PainelComponent
+  ],
+  imports: [
+    PainelComponent
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class PainelModule { }
